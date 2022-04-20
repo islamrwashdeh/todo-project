3bb55525c47ddc0704583393d1a410d07e79099e
sal14ma <islamrwashdeh@yahoo.com>
2022-04-21 01:11:48 +0300

    add

--- app.js
+++ app.js
@@ -1,28 +1,58 @@
 
-var userName= prompt("pleas enter your name");
-var gender= prompt("pleas enter your type");
-var age= prompt("pleas enter your age ");
-if (age<=0){
-    alert("what are you doing ?? enter your reall age")
-    var age= prompt("pleas enter your age ")
-}
+// var userName= prompt("pleas enter your name");
+// var gender= prompt("pleas enter your type");
+// var age= prompt("pleas enter your age ");
+// if (age<=0){
+//     alert("what are you doing ?? enter your reall age")
+//     var age= prompt("pleas enter your age ")
+// }
 
-var welcomeMasseg =confirm("do you whant to skip the welcoming message? ")
+// var welcomeMasseg =confirm("do you whant to skip the welcoming message? ")
    
-if(welcomeMasseg=true){
+// if(welcomeMasseg=true){
     
-    alert("sory i dont know how to do that so keep going ")}
+//     alert("sory i dont know how to do that so keep going ")}
 
 
-if (gender ==="male"){
-var welcomingMessage2 =alert("helow Mr"+"  " +userName)
+// if (gender ==="male"){
+// var welcomingMessage2 =alert("helow Mr"+"  " +userName)
+// }
+
+// else if 
+//     (gender ==="female"){
+//       var welcomingMessage2 =alert("helow MS"+"  " +userName)
+// }
+// else{
+//     (gender ==="female" ||gender ==="male" )
+//         var welcomingMessage2 =alert("helow "+"  " +userName+" well done")
+// }
+
+'use stric';
+let inputValue=[];
+
+function answrOne(){
+    inputValue.push(prompt("do you like js?"));
 }
 
-else if 
-    (gender ==="female"){
-      var welcomingMessage2 =alert("helow MS"+"  " +userName)
+function answrTow(){
+    inputValue.push(prompt("do you like problem solving ?"));
 }
-else{
-    (gender ==="female" ||gender ==="male" )
-        var welcomingMessage2 =alert("helow "+"  " +userName+" well done")
+function answrThree(){
+    inputValue.push(prompt("do love coding ?"));
 }
+
+answrOne();
+answrTow();
+answrThree();
+
+function theAnswer(){
+    for (let i=0; i<inputValue.length;i++) {
+        if (inputValue[i]=== ""){
+            caches.log("invalid");
+        }
+        else {
+            console.log(inputValue[i])
+        }
+    }
+}
+inputValue();
